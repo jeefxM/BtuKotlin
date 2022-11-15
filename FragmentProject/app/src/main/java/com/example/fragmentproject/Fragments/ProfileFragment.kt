8ffd0e4aa3f7a57013f5151900e60623fced68e6
jeefxM <87044530+jeefxM@users.noreply.github.com>
2022-11-15 14:59:24 +0400

    Add files via upload

---
+++ FragmentProject/app/src/main/java/com/example/fragmentproject/Fragments/ProfileFragment.kt
@@ -0,0 +1,25 @@
+package com.example.fragmentproject.Fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.fragmentproject.R
+
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+
+
+}
+
+
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//
+//        val view = inflater.inflate(R.layout.fragment_profile, container, true)
+//
+//        return view
+//    }
